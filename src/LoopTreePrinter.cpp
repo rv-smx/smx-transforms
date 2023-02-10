77c44d8c92fbc18a693166e9698a37a03cb0b34d
MaxXSoft <x@MaxXSoft.net>
2023-02-10 20:56:09 +0800

    (LoopTreePrinter) simplified the output data format

--- src/LoopTreePrinter.cpp
+++ src/LoopTreePrinter.cpp
@@ -1,7 +1,12 @@
 #include "LoopTreePrinter.h"
 
 #include <cstddef>
+#include <cstdlib>
+#include <string>
 
+#include "llvm/ADT/Optional.h"
+#include "llvm/ADT/SmallSet.h"
+#include "llvm/ADT/StringMap.h"
 #include "llvm/Analysis/LoopInfo.h"
 
 #include "Utils.h"
@@ -10,34 +15,64 @@ using namespace llvm;
 
 namespace {
 
-void scanLoops(raw_ostream &OS, bool &First, Loop *Current, Loop *Parent) {
-  // Print comma.
-  if (First) {
-    First = false;
-  } else {
-    OS << ',';
+/// Set of loop IDs.
+using LoopIdSet = SmallSet<Optional<std::string>, 1>;
+
+/// Map for storing pairs of loop ID and the corresponding loop ID set.
+using LoopParentMap = StringMap<LoopIdSet>;
+
+/// Builds the loop parent map by the given loop.
+void buildLoopParentMap(LoopParentMap &Pairs, const Loop *L) {
+  // Gets loop ID of the current loop and its parent.
+  auto Id = convPrintableToStr(L->getStartLoc());
+  Optional<std::string> ParentId;
+  if (auto Parent = L->getParentLoop()) {
+    ParentId = convPrintableToStr(Parent->getStartLoc());
   }
 
-  // Print loop tree information.
-  printPrintable(OS, Current->getStartLoc());
-  OS << ":{\"parent\":";
-  if (Parent) {
-    printPrintable(OS, Parent->getStartLoc());
-  } else {
-    OS << "null";
-  }
-  OS << ",\"children\":[";
-  const auto &SubLoops = Current->getSubLoops();
-  for (std::size_t i = 0; i < SubLoops.size(); ++i) {
-    if (i)
-      OS << ',';
-    printPrintable(OS, SubLoops[i]->getStartLoc());
-  }
-  OS << "]}";
+  // Insert to the map.
+  if (ParentId != Id)
+    Pairs[Id].insert(ParentId);
 
   // Scan for all sub loops.
-  for (auto SubLoop : SubLoops)
-    scanLoops(OS, First, SubLoop, Current);
+  for (auto SubLoop : L->getSubLoops())
+    buildLoopParentMap(Pairs, SubLoop);
+}
+
+/// Prints loop IDs in the given loop ID set to the output stream.
+void printLoopIds(raw_ostream &OS, const LoopIdSet &Ids) {
+  OS << '[';
+  bool First = true;
+  for (const auto &Id : Ids) {
+    if (First) {
+      First = false;
+    } else {
+      OS << ',';
+    }
+    if (Id) {
+      printString(OS, *Id);
+    } else {
+      OS << "null";
+    }
+  }
+  OS << ']';
+}
+
+/// Prints pairs in the given loop parent map to the output stream.
+void printLoopParentPairs(raw_ostream &OS, const LoopParentMap &Pairs) {
+  OS << '{';
+  bool First = true;
+  for (const auto &It : Pairs) {
+    if (First) {
+      First = false;
+    } else {
+      OS << ',';
+    }
+    printString(OS, It.getKey());
+    OS << ':';
+    printLoopIds(OS, It.getValue());
+  }
+  OS << '}';
 }
 
 } // namespace
@@ -46,12 +81,14 @@ PreservedAnalyses LoopTreePrinter::run(Function &F,
                                        FunctionAnalysisManager &FAM) const {
   auto &LI = FAM.getResult<LoopAnalysis>(F);
 
-  // Scan for all loops.
-  OS << '{';
-  bool First = true;
+  // Build loop parent map.
+  LoopParentMap Pairs;
   for (auto Loop : LI)
-    scanLoops(OS, First, Loop, nullptr);
-  OS << "}\n";
+    buildLoopParentMap(Pairs, Loop);
+
+  // Print to the output stream.
+  printLoopParentPairs(OS, Pairs);
+  OS << '\n';
 
   return PreservedAnalyses::all();
 }
