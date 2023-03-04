316690a2ce6191fa5d8bdc02f8aa67963665cf46
MaxXSoft <x@MaxXSoft.net>
2023-03-04 15:46:55 +0800

    (SMXPass) removed pass `SMXPass`

--- src/PassRegistry.cpp
+++ src/PassRegistry.cpp
@@ -3,7 +3,6 @@
 #include "LoopProfiler.h"
 #include "LoopStreamization.h"
 #include "LoopTreePrinter.h"
-#include "SMXPass.h"
 #include "StreamMemoryAnalysis.h"
 
 using namespace llvm;
@@ -11,7 +10,6 @@ using namespace llvm;
 PassPluginLibraryInfo getSMXTransformsPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "SMXTransforms", LLVM_VERSION_STRING,
           [](PassBuilder &PB) {
-            registerSMXPass(PB);
             registerStreamMemoryAnalysis(PB);
             registerLoopProfiler(PB);
             registerLoopTreePrinter(PB);
